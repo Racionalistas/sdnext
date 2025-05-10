f846a88c0a431c031dd1decfeaaf802375fc9561
Александр Приходько <108543198+Racionalistas@users.noreply.github.com>
2025-05-11 02:24:02 +0300

    fix start props

--- start.sh
+++ start.sh
@@ -11,9 +11,9 @@ source venv/bin/activate
 # 1) стартуем SD.Next WebUI в API‑only режиме
 echo "==== Starting SD.Next WebUI (API only) ===="
 bash webui.sh \
-  --api --listen --port 7860 --debug --use-cuda --models-dir /mnt/models --skip-version-check --skip-torch-cuda-test --skip-python-version-check \
-  --ckpt /mnt/models/Stable-diffusion/photon_v1.safetensors --disable-console-progressbars --no-download-sd-model --disable-safe-unpickle \
-  --api-log --log sdnext.log --quick --enable-extensions controlnet &
+  --api --listen --port 7860 --debug --use-cuda --models-dir /mnt/models \
+  --ckpt /mnt/models/Stable-diffusion/photon_v1.safetensors --disable-console-progressbars --disable-safe-unpickle \
+  --api-log --log sdnext.log --quick --extensions-dir /mnt/extensions &
 WEBUI_PID=$!
 
 # 2) ждём, пока поднимутся модели
